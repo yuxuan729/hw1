97d0e49daaf05cf44236323b6d41746760ebad4d
yuxuan729 <394079376@qq.com>
2022-07-02 16:41:25 +0800

    fix delete

--- delete.py
+++ delete.py
@@ -1,9 +1,55 @@
 import numpy as np
+from typing import *
 
-a = np.random.randint(0,2,(3,4))
-b = np.random.randint(0,2,(4,3))
-print(a)
-print(b)
 
-print(a@b)
-print(np.dot(a,b))
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1,-1]
+        left = self.get_left_index(nums,target)
+        right = self.get_right_index(nums, target)
+        return [left,right]
+
+    def get_left_index(self,nums: List[int], target: int):
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                right = mid-1
+            if nums[mid] > target:
+                right = mid-1
+            if nums[mid] < target:
+                left = mid+1
+        if left < len(nums) and nums[left] == target:
+            return left
+        else:
+            return -1
+
+    def get_right_index(self,nums: List[int], target: int):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
+            if nums[mid] < target:
+                left = mid + 1
+        if right >= 0 and nums[right] == target:
+            return right
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    nums1 = [2,2]
+    target1 = 3                      # [3, 4]
+    s = Solution()
+    ret1 = s.searchRange(nums1,target1)
+    print(ret1)
+
+
+
+
